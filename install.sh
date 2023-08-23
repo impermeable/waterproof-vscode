7f75701b71224d56978dad8cc5bdf0d0c2c68757
Jim Portegies <j.w.portegies@tue.nl>
2023-08-23 17:33:40 +0200

    Split scripts

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #! /usr/bin/bash
 
-# Add the repository to get opam from and install it
-add-apt-repository ppa:avsm/ppa
-apt update
-apt install opam
-
 # Speed up compilation later
 export MAKEFLAGS='-j 10'
 
 # Install OCaml
+opam init
+eval $(opam env)
 opam switch create waterproof --packages coq.8.17.0
 $ eval $(opam env --switch=waterproof)
 
---
+++ setup-opam.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+# Add the repository to get opam from and install it
+add-apt-repository ppa:avsm/ppa
+apt update
+apt install opam

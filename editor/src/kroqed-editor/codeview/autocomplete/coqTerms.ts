16e04b856c010d1715a89c18fe242be8dc0128e5
DikieDick <d.h.l.n.arends@student.tue.nl>
2023-09-03 12:21:44 +0200

    Autocomplete: New regular expression for coq completions.

--- editor/src/kroqed-editor/codeview/autocomplete/coqTerms.ts
+++ editor/src/kroqed-editor/codeview/autocomplete/coqTerms.ts
@@ -7,16 +7,14 @@ const coqCompletions: Completion[] = coqWords;
 
 export const coqCompletionSource: CompletionSource = function(context: CompletionContext): Promise<CompletionResult | null> {
     return new Promise((resolve, reject) => {
-        // TODO: This RegEx might be suboptimal.
-        let before = context.matchBefore(/([^])/);
+        let before = context.matchBefore(/\w/);
         // If completion wasn't explicitly started and there
         // is no word before the cursor, don't open completions.
         if (!context.explicit && !before) resolve(null);
         resolve({
             from: before ? before.from : context.pos,
             options: coqCompletions,
-            // TODO: The validFor field here could be optimized. 
-            validFor: /^\w*$/
+            validFor: /[^ ]*/
         });
     });
 }

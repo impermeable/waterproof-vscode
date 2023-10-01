5928963591b22f86e09bd2da0c95abe32227b11b
DikieDick <d.h.l.n.arends@student.tue.nl>
2023-10-01 09:09:24 +0200

    Custom new file command (#25)
    * Extension: Create custom 'new file' command.
    
    Works similar to the 'New Jupyter Notebook' command.
    
    * Extension: Update new file contents

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,8 @@ import {
     TextDocument,
     WorkspaceConfiguration,
     commands,
-    workspace
-} from "vscode";
+    workspace,
+    window} from "vscode";
 import { LanguageClientOptions, RevealOutputChannelOn } from "vscode-languageclient";
 
 import { IExecutor, IGoalsComponent, IStatusComponent } from "./components";
@@ -24,6 +24,8 @@ import { CommonExecute } from "./webviews/standardviews/commonExecute";
 import { ExecutePanel } from "./webviews/standardviews/execute";
 import { SymbolsPanel } from "./webviews/standardviews/symbols";
 import { TacticsPanel } from "./webviews/standardviews/tactics";
+
+import { newFileContent } from "./constants";
 
 /**
  * Main extension class
@@ -139,6 +141,30 @@ export class Coqnitive implements Disposable {
         this.registerCommand("restart", this.restartClient);
         this.registerCommand("toggle", this.toggleClient);
         this.registerCommand("stop", this.stopClient);
+
+        // Register the new Waterproof Document command
+        this.registerCommand("newWaterproofDocument", this.newFileCommand);
+    }
+
+    /**
+     * Handle the newWaterproofDocument command. 
+     * This command can be either activated by the user via the 'command palette' 
+     * or by using the File -> New File... option. 
+     */
+    private async newFileCommand(): Promise<void> {
+
+        window.showSaveDialog({filters: {'Waterproof': ["mv", "v"]}, title: "New Waterproof Document"}).then((uri) => {
+            if (!uri) {
+                window.showErrorMessage("Something went wrong in creating a new waterproof document");
+                return;
+            }
+
+            workspace.fs.writeFile(uri, Buffer.from(newFileContent)).then(() => {
+                // Open the file using the waterproof editor
+                // TODO: Hardcoded `coqEditor.coqEditor`.
+                commands.executeCommand("vscode.openWith", uri, "coqEditor.coqEditor");
+            });
+        });
     }
 
     /**

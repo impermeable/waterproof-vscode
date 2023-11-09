ae1c1250295d73a0f6eb2e5be338be6b34d0b1ea
DikieDick <d.h.l.n.arends@student.tue.nl>
2023-11-09 08:45:11 +0100

    Store symbol/tactic data in shared location. (#44)
    * Autocomplete: Use JSON file for icons.
    
    * Symbols Panel: Use same source JSON as autocomplete.
    
    * Completions: Update information about the shared `json` files.
    
    * Completions: Move tactics to shared `json` file.
    
    This unifies the two `json` files that are used for completions, in
    the autocomplete within the editor as well as the tactics panel.
    
    * Tactics Panel: Convert inserted tactic into snippet.
    
    This now offers the same experience as when using autocompleted
    tactics.
    
    * changed coqSyntax name and fixed some broken imports
    
    * Completions: Use template to start snippet completion
    
    * Completions: Add correct examples/descriptions to tactics file
    
    * fixed tactics.json missing template
    
    ---------
    
    Co-authored-by: diego <driverag22@gmail.com>

--- editor/src/kroqed-editor/codeview/nodeview.ts
+++ editor/src/kroqed-editor/codeview/nodeview.ts
@@ -1,5 +1,5 @@
-import { Completion, CompletionContext, CompletionResult, CompletionSource, autocompletion } from "@codemirror/autocomplete";
-import { indentWithTab } from "@codemirror/commands";
+import { Completion, CompletionContext, CompletionResult, CompletionSource, autocompletion, snippet } from "@codemirror/autocomplete";
+import { defaultKeymap, indentWithTab } from "@codemirror/commands";
 import { defaultHighlightStyle, syntaxHighlighting } from "@codemirror/language";
 import { coq, coqSyntaxHighlighting } from "./lang-pack"
 import { Compartment, EditorState, Extension } from "@codemirror/state"
@@ -10,7 +10,7 @@ import {
 import { Node, Schema } from "prosemirror-model"
 import { EditorView } from "prosemirror-view"
 import { customTheme } from "./color-scheme"
-import { symbolCompletionSource, coqCompletionSource, tacticCompletionSource } from "./autocomplete";
+import { symbolCompletionSource, coqCompletionSource, tacticCompletionSource, renderIcon } from "../autocomplete";
 import { EmbeddedCodeMirrorEditor } from "../embedded-codemirror";
 import { linter, LintSource, Diagnostic } from "@codemirror/lint";
 
@@ -63,7 +63,9 @@ export class CodeBlockView extends EmbeddedCodeMirrorEditor {
 						this.dynamicCompletionSource, 
 						symbolCompletionSource, 
 						coqCompletionSource
-					]
+					], 
+					icons: false,
+					addToOptions: [renderIcon]
 				}),
 				cmKeymap.of([
 					indentWithTab,
@@ -90,6 +92,14 @@ export class CodeBlockView extends EmbeddedCodeMirrorEditor {
 
 		this.updating = false;
 		this.handleNewComplete([]);
+	}
+
+	public handleSnippet(template: string, posFrom: number, posTo: number) {
+		this._codemirror.focus();
+		snippet(template)({
+			state: this._codemirror.state,
+			dispatch: this._codemirror.dispatch
+		}, null, posFrom, posTo);
 	}
 
 	private lintingFunction: LintSource = (view: CodeMirror): readonly Diagnostic[] => {

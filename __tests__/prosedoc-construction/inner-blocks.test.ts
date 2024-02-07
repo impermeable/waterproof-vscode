e440497cc31fe2ef058f6f8d6f247b142fa060ea
DikieDick <d.h.l.n.arends@student.tue.nl>
2024-02-07 11:27:47 +0100

    Add tests for inner block extraction

---
+++ __tests__/prosedoc-construction/inner-blocks.test.ts
@@ -0,0 +1,61 @@
+import { createCoqDocInnerBlocks, createCoqInnerBlocks, createInputAndHintInnerBlocks } from "../../editor/src/kroqed-editor/prosedoc-construction/blocks/inner-blocks";
+import { isCoqBlock, isCoqCodeBlock, isCoqDocBlock, isCoqMarkdownBlock, isMarkdownBlock, isMathDisplayBlock } from "../../editor/src/kroqed-editor/prosedoc-construction/blocks/typeguards";
+
+test("Inner input area (and hint) blocks", () => {
+    const inputAreaContent = "$$1028 + 23 = ?$$\n```coq\nCompute 1028 + 23.\n```";
+    
+    const blocks = createInputAndHintInnerBlocks(inputAreaContent);
+    
+    // FIXME: Should really be 2.
+    expect(blocks.length).toBe(3);
+    expect(isMathDisplayBlock(blocks[0])).toBe(true);
+    // FIXME: There is a markdown block here with only a newline.
+    expect(isMarkdownBlock(blocks[1])).toBe(true);
+    expect(blocks[1].stringContent).toBe("\n");
+    expect(isCoqBlock(blocks[2])).toBe(true);
+
+    // Math display content:
+    expect(blocks[0].stringContent).toBe("1028 + 23 = ?");
+    expect(blocks[0].range.from).toBe(0);
+    expect(blocks[0].range.to).toBe(17);
+
+    // CoqBlock content:
+    expect(blocks[2].stringContent).toBe("Compute 1028 + 23.");
+    expect(blocks[2].range.from).toBe(18);
+    expect(blocks[2].range.to).toBe(inputAreaContent.length);
+});
+
+test("Inner coq blocks", () => {
+    const coqContent = "Compute 1 + 1.\n(** * Header *)";
+    const blocks = createCoqInnerBlocks(coqContent);
+
+    // One block for the coq content and one block for the comment.
+    expect(blocks.length).toBe(2);
+    expect(isCoqCodeBlock(blocks[0])).toBe(true);
+    // FIXME: What should we do with the trailing newline?
+    expect(blocks[0].stringContent).toBe("Compute 1 + 1.");
+    expect(blocks[0].range.from).toBe(0);
+    expect(blocks[0].range.to).toBe(15);
+
+    expect(isCoqDocBlock(blocks[1])).toBe(true);
+    expect(blocks[1].stringContent).toBe("* Header");
+    expect(blocks[1].range.from).toBe(15);
+    expect(blocks[1].range.to).toBe(coqContent.length);
+    
+    expect(blocks[1].innerBlocks?.length).toBe(1);
+    expect(isCoqMarkdownBlock(blocks[1].innerBlocks![0])).toBe(true);
+});
+
+test("Inner Coqdoc blocks", () => {
+    const coqdocContent = "* Header\n$\\text{math display}$";
+    const blocks = createCoqDocInnerBlocks(coqdocContent);
+    expect(blocks.length).toBe(2);
+    expect(isCoqMarkdownBlock(blocks[0])).toBe(true);
+    // FIXME: Again the trailing newline.
+    expect(blocks[0].stringContent).toBe("* Header\n");
+    expect(blocks[0].range).toStrictEqual({ from: 0, to: 9 });
+
+    expect(isMathDisplayBlock(blocks[1])).toBe(true);
+    expect(blocks[1].stringContent).toBe("\\text{math display}");
+    expect(blocks[1].range).toStrictEqual({ from: 9, to: coqdocContent.length });
+});

3e6b0dc014c9b656b19ad7009071c956475a4de4
DikieDick <26772815+DikieDick@users.noreply.github.com>
2024-06-08 17:48:36 +0200

    Crude version of new grammar

--- editor/src/kroqed-editor/codeview/lang-pack/index.ts
+++ editor/src/kroqed-editor/codeview/lang-pack/index.ts
@@ -4,48 +4,41 @@ import {
     HighlightStyle, LRLanguage, LanguageSupport, delimitedIndent, foldInside, foldNodeProp,
     indentNodeProp, syntaxHighlighting
 } from "@codemirror/language"
-import { Tag, styleTags, tags as t } from "@lezer/highlight"
+import { Tag, styleTags, tags as t, tagHighlighter } from "@lezer/highlight"
 
 // Importing the parser for the Coq language
 import { parser } from "./syntax"
 
 // Defining custom tags for specific elements of the Coq language
 let customTags = {
-    vernacular: Tag.define(),
-    gallina: Tag.define()
+    waterproofTactic: Tag.define(),
+    coqSentence: Tag.define(),
 }
 
 // Highlighting specific elements of the Coq language
 export let highlight = HighlightStyle.define([
-    { tag: customTags.gallina, color: "#6637dd" },
-    { tag: customTags.vernacular, color: "#7872d0" }
+    { tag: customTags.waterproofTactic, color: "#0000FF" },
+    { tag: customTags.coqSentence, color: "#FF0000" },
+    { tag: t.comment, color: "#00FF00" },
 ])
 
 // Defining the Coq language syntax, highlighting and indentation
 export const coqLanguage = LRLanguage.define({
     parser: parser.configure({
         props: [
-            indentNodeProp.add({
-                Application: delimitedIndent({ closing: ")", align: false })
-            }),
-            foldNodeProp.add({
-                Application: foldInside
-            }),
+            // indentNodeProp.add({
+            //     Application: delimitedIndent({ closing: ")", align: false })
+            // }),
+            // foldNodeProp.add({
+            //     Application: foldInside
+            // }),
             styleTags({
-                Identifier: t.variableName,
-                Boolean: t.bool,
-                String: t.string,
-                BlockComment: t.blockComment,
-                "( )": t.paren,
-                // extra words
-                Gallina: customTags.gallina,
-                Vernacular: customTags.vernacular,
+                WaterproofTactic: customTags.waterproofTactic,
+                CoqSentence: customTags.coqSentence,
+                Comment: t.comment
             })
         ]
-    }),
-    languageData: {
-        commentTokens: { line: "(*" }
-    }
+    })
 })
 
 export function coq() {

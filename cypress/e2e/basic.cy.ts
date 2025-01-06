f50c9444bf5b6f23e4e2c499fa4427807bc6babe
DikieDick <26772815+DikieDick@users.noreply.github.com>
2025-01-06 11:20:32 +0100

    Testing: Add Cypress testing framework
    I have added a very limited selection of tests, but will write
    documentation about how the tests can be expanded.

---
+++ cypress/e2e/basic.cy.ts
@@ -0,0 +1,89 @@
+
+const edits = [];
+
+describe('Basic tests', () => {
+  beforeEach(() => {
+    cy.visit("../../__test_harness/index.html", {
+      onBeforeLoad: (window) => {
+        // Supply a 'fake' acquireVsCodeApi function
+        window.acquireVsCodeApi = function () {
+          return {
+            postMessage: (msg) => {
+              if (msg.type === "ready") {
+                window.postMessage({
+                  type:"init",
+                  body: {
+                    value: "# Title\n```coq\nDefinition foo := 42.\n```\n",
+                    format: "MarkdownV",
+                    version: 1,
+                  }
+                });
+              } else if (msg.type === "docChange") {
+                edits.push(msg.body);
+              }
+            }
+          }
+        }
+      }
+    });
+  })
+
+  it('Editor opens, contains all parts and displays file', () => {
+    // Editor is visible
+    cy.get("#editor").should("be.visible");
+    // Menubar is visible
+    cy.get(".menubar").should("be.visible");
+    
+    // Progress bar is visible
+    // TODO: Progress bar only appears after clicking in the editor?
+    // cy.get(".progress-bar").should("be.visible");
+    
+    // Editor should have a H1 (markdown header) and a .cm-content (coq code)
+    cy.get("#editor h1").should("be.visible");
+    cy.get("#editor .cm-content").should("be.visible");
+
+    // Teacher mode is disabled, we should not be able to interact with 
+    // the markdown or coq code
+    cy.get("#editor h1").click(); 
+    // Should **not** have changed the H1 into markdown editor
+    cy.get("#editor h1").should("be.visible");
+
+    // TODO: This also immediately opens the markdown editor for the H1
+    cy.window().then((win) => { win.postMessage({type: "toggleTeacherMode", body: true}) });
+    cy.get('coqblock > .cm-editor > .cm-scroller > .cm-content').click(); // to reset h1
+    cy.get("#editor h1").should("be.visible");
+    cy.get("#editor h1").click();
+    cy.get(".markdown-view").should("be.visible");
+  });
+
+  it("Make basic edits to md and coq", () => {
+    cy.window().then((win) => { win.postMessage({type: "toggleTeacherMode", body: true}) });
+
+    cy.get(".markdown-view").type("\n## Hello World");
+    cy.get(".markdown-view").should("contain.text", "Hello World");
+    cy.get('coqblock > .cm-editor > .cm-scroller > .cm-content').click(); // to reset h1
+    cy.get("H2").should("exist");
+
+    // We record edits in the 'edits' global variable
+    //   this way we can check properties of the edits here
+    cy.wrap(edits).then((edits) => {
+      expect(edits.length).to.equal(15);
+
+      // expect edits startInFile to be in increasing order
+      expect(edits).to.satisfy((edits) => 
+        edits.every((edit, i, arr) => 
+          // startInFile equals endInFile (insertions)
+          edit.startInFile === edit.endInFile  
+          && 
+          // startInFile's are in increasing order (consecutive)
+          (i === 0 || arr[i-1].startInFile < edit.startInFile)
+        )
+      );
+
+      // Flatten the edits finalText into one string
+      const insertedText = edits.map(e => e.finalText).join("");
+      expect(insertedText).to.equal("\n## Hello World");
+    });
+
+  });
+})

490a55934372aaa01b28a94a3d5c535f7f6c42a6
Pim Otte <p.j.otte@uu.nl>
2025-01-15 14:38:36 +0100

    Fix config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,21 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+const config = tseslint.config(
   {
-    ignores : ["editor_output/", "out/"]
+    ignores: ["editor_output/", "out/", "test_out/"],
+  },
+  {
+   extends: [
+     eslint.configs.recommended,
+     tseslint.configs.recommended,
+   ],
+   rules: {
+       "@typescript-eslint/no-unused-vars": ["error", {
+         "varsIgnorePattern": "^_",
+         "argsIgnorePattern": "^_"
+       }],
+     },
   }
 );
+export default config;

d0013a70a01198a6445a3bd30b7a36b19eb702fa
M-J-V <malcolmvas@gmail.com>
2025-07-20 18:51:30 +0200

    Update the dynamic light/dark highlighting with new grammar

--- editor/src/kroqed-editor/codeview/lang-pack/index.ts
+++ editor/src/kroqed-editor/codeview/lang-pack/index.ts
@@ -9,47 +9,63 @@ import { Tag, styleTags, tags as t } from "@lezer/highlight"
 import { parser } from "./syntax"
 
 // Defining custom tags for specific elements of the Coq language
-const customTags = {
-    vernacular: Tag.define(),
-    gallina: Tag.define()
+let customTags = {
+    waterproofTactic: Tag.define(),
+    tacticInput: Tag.define(),
+    takeInput: Tag.define(),
+    lemmaStatement: Tag.define(),
+    lemma: Tag.define(),
+    comment: Tag.define(),
+    proofQed: Tag.define()
 }
 
 // Highlighting specific elements of the Coq language
 export const highlight_dark = HighlightStyle.define([
-    { tag: customTags.gallina, color: "#cc00ffff" },
-    { tag: customTags.vernacular, color: "#00FF00" }
+    { tag: customTags.waterproofTactic, color: "#5EC300" },
+    { tag: customTags.tacticInput, color: "#FF0000" },
+    { tag: customTags.lemmaStatement, color: "#DB6B16" },
+    { tag: customTags.lemma, color: "#FF00F7" },
+    { tag: customTags.comment, color: "#0000FF" },
+    { tag: customTags.proofQed, color: "#FFE100" },
 ])
 
 export const highlight_light = HighlightStyle.define([
-    { tag: customTags.gallina, color: "#FF0000" },
-    { tag: customTags.vernacular, color: "#0000FF" }
+    { tag: customTags.waterproofTactic, color: "#0300c3ff" },
+    { tag: customTags.tacticInput, color: "#1dc21dff" },
+    { tag: customTags.lemmaStatement, color: "#b31884ff" },
+    { tag: customTags.lemma, color: "#b32525ff" },
+    { tag: customTags.comment, color: "#aab335ff" },
+    { tag: customTags.proofQed, color: "#2080a7ff" },
 ])
 
 // Defining the Coq language syntax, highlighting and indentation
 export const coqLanguage = LRLanguage.define({
     parser: parser.configure({
         props: [
-            indentNodeProp.add({
-                Application: delimitedIndent({ closing: ")", align: false })
-            }),
-            foldNodeProp.add({
-                Application: foldInside
-            }),
+            // indentNodeProp.add({
+            //     Application: delimitedIndent({ closing: ")", align: false })
+            // }),
+            // foldNodeProp.add({
+            //     Application: foldInside
+            // }),
             styleTags({
-                Identifier: t.variableName,
-                Boolean: t.bool,
-                String: t.string,
-                BlockComment: t.blockComment,
-                "( )": t.paren,
-                // extra words
-                Gallina: customTags.gallina,
-                Vernacular: customTags.vernacular,
+                WaterproofTactic: customTags.waterproofTactic,
+                TacticInput: customTags.tacticInput,
+                Lemma: customTags.lemma,
+                Comment: customTags.comment,
+                ProofandQed: customTags.proofQed,
+                LemmaStatement: customTags.lemmaStatement,
+                // Also update each of the middle tokens to match the tactics
+                TacticMiddleByOrSince: customTags.waterproofTactic,
+                TacticMiddleExpand: customTags.waterproofTactic,
+                TacticMiddleObtain: customTags.waterproofTactic,
+                TacticMiddleDefine: customTags.waterproofTactic,
+                TacticMiddleBecauseFirst: customTags.waterproofTactic,
+                TacticMiddleBecauseSecond: customTags.waterproofTactic,
+                TacticMiddleEither: customTags.waterproofTactic
             })
         ]
-    }),
-    languageData: {
-        commentTokens: { line: "(*" }
-    }
+    })
 })
 
 export function coq() {

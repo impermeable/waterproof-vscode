e61cdab120765e0a6c3f1583b49fad2931469fee
DikieDick <26772815+DikieDick@users.noreply.github.com>
2025-08-25 11:35:56 +0200

    Disable specific eslint error in mv-mapping test

--- __tests__/mv-mapping.test.ts
+++ __tests__/mv-mapping.test.ts
@@ -1,4 +1,5 @@
-
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// FIXME: Remove this and linting fails, change the ts-ignore to ts-expect-error and the unit tests fail
 import { ReplaceStep, WaterproofSchema, FileTranslator } from "waterproof-editor";
 import { TextDocMappingMV as TextDocMapping } from "../editor/src/mapping";
 import { expect } from "@jest/globals";
@@ -88,7 +89,7 @@ test("testMapping", () => {
     const textDocMapping = new TextDocMapping(`<markdown>Hello</markdown>`, 0)
     const mapping = textDocMapping.getMapping()
     if (mapping.has(3-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(3-2).startProse).toBe(3-2);expect(mapping.get(3-2).endProse).toBe(8-2);expect(mapping.get(3-2).startText).toBe(0);expect(mapping.get(3-2).endText).toBe(5)
     } else {
         throw new Error("Index does not exist")
@@ -99,7 +100,7 @@ test("testMapping", () => {
     const textDocMapping = new TextDocMapping(`<coqblock prePreWhite="newLine" postPreWhite="newLine" prePostWhite="newLine" postPostWhite="newLine"><coqcode>This is code</coqcode></coqblock>`, 0)
     const mapping = textDocMapping.getMapping()
     if (mapping.has(4-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(4-2).startProse).toBe(4-2);expect(mapping.get(4-2).endProse).toBe(16-2);expect(mapping.get(4-2).startText).toBe(8);expect(mapping.get(4-2).endText).toBe(20)
     } else {
         throw new Error("Index does not exist")
@@ -110,13 +111,13 @@ test("testMapping 2", () => {
     const textDocMapping = new TextDocMapping(`<markdown>Hello</markdown><coqblock prePreWhite="newLine" postPreWhite="newLine" prePostWhite="newLine" postPostWhite="newLine"><coqcode>This is code</coqcode></coqblock>`, 0)
     const mapping = textDocMapping.getMapping()
     if (mapping.has(3-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(3-2).startProse).toBe(3-2);expect(mapping.get(3-2).endProse).toBe(8-2);expect(mapping.get(3-2).startText).toBe(0);expect(mapping.get(3-2).endText).toBe(5)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(11-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(11-2).startProse).toBe(11-2);expect(mapping.get(11-2).endProse).toBe(23-2);expect(mapping.get(11-2).startText).toBe(13);expect(mapping.get(11-2).endText).toBe(25)
     } else {
         throw new Error("Index does not exist")
@@ -128,13 +129,13 @@ test("testMapping 3", () => {
     const textDocMapping = new TextDocMapping(`<coqblock prePreWhite="newLine" postPreWhite="newLine" prePostWhite="newLine" postPostWhite="newLine"><coqcode>This is code</coqcode></coqblock><markdown>Hello</markdown>`,0)
     const mapping = textDocMapping.getMapping()
     if (mapping.has(4-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(4-2).startProse).toBe(4-2);expect(mapping.get(4-2).endProse).toBe(16-2);expect(mapping.get(4-2).startText).toBe(8);expect(mapping.get(4-2).endText).toBe(20)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(19-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(19-2).startProse).toBe(19-2);expect(mapping.get(19-2).endProse).toBe(24-2);expect(mapping.get(19-2).startText).toBe(25);expect(mapping.get(19-2).endText).toBe(30)
     } else {
         throw new Error("Index does not exist")
@@ -146,7 +147,7 @@ test("testMapping 4", () => {
     const textDocMapping = new TextDocMapping(`<coqblock prePreWhite="newLine" postPreWhite="newLine" prePostWhite="newLine" postPostWhite="newLine"><coqdoc preWhite="newLine" postWhite="newLine"><coqdown>This is code</coqdown></coqdoc></coqcode></coqblock>`,0)
     const mapping = textDocMapping.getMapping()
     if (mapping.has(5-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(5-2).startProse).toBe(5-2);expect(mapping.get(5-2).endProse).toBe(17-2);expect(mapping.get(5-2).startText).toBe(13);expect(mapping.get(5-2).endText).toBe(25)
     } else {
         throw new Error("Index does not exist")
@@ -160,43 +161,43 @@ test("docString", () => {
     const textDocMapping = new TextDocMapping(translated,0)
     const mapping = textDocMapping.getMapping()
     if (mapping.has(3-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(3-2).startProse).toBe(3-2);expect(mapping.get(3-2).endProse).toBe(8-2);expect(mapping.get(3-2).startText).toBe(0);expect(mapping.get(3-2).endText).toBe(5)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(11-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(11-2).startProse).toBe(11-2);expect(mapping.get(11-2).endProse).toBe(23-2);expect(mapping.get(11-2).startText).toBe(13);expect(mapping.get(11-2).endText).toBe(25)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(27-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(27-2).startProse).toBe(27-2);expect(mapping.get(27-2).endProse).toBe(32-2);expect(mapping.get(27-2).startText).toBe(42);expect(mapping.get(27-2).endText).toBe(47)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(35-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(35-2).startProse).toBe(35-2);expect(mapping.get(35-2).endProse).toBe(36-2);expect(mapping.get(35-2).startText).toBe(60);expect(mapping.get(35-2).endText).toBe(61)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(39-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(39-2).startProse).toBe(39-2);expect(mapping.get(39-2).endProse).toBe(48-2);expect(mapping.get(39-2).startText).toBe(84);expect(mapping.get(39-2).endText).toBe(93)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(51-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(51-2).startProse).toBe(51-2);expect(mapping.get(51-2).endProse).toBe(52-2);expect(mapping.get(51-2).startText).toBe(100);expect(mapping.get(51-2).endText).toBe(101)
     } else {
         throw new Error("Index does not exist")
     }
     if (mapping.has(55-2)) {
-        // @ts-expect-error FIXME: calling .get on the mapping can result in `undefined`
+        //@ts-ignore
         expect(mapping.get(55-2).startProse).toBe(55-2);expect(mapping.get(55-2).endProse).toBe(67-2);expect(mapping.get(55-2).startText).toBe(109);expect(mapping.get(55-2).endText).toBe(121)
     } else {
         throw new Error("Index does not exist")

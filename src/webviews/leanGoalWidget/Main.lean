f8b2c7728154c4b2ece2e1f7f2cf14c851ed02c1
Milkiniss <m.stefanova@student.tue.nl>
2025-10-24 02:36:47 +0200

    update UI of goal widget

--- src/webviews/leanGoalWidget/Main.lean
+++ src/webviews/leanGoalWidget/Main.lean
@@ -32,38 +32,3 @@ Proof:
   Let's prove that b - a + 1 works
   We compute
 QED
-
-Example "Using cases to prove max"
-  Given:
-  Assume:
-  Conclusion: ∀ (x y: ℝ), (max x y) = x ∨ (max x y) = y
-Proof:
-  with_panel_widgets [GoalTypePanel]
-  Fix x y
-  We proceed using cases on the comparison between x and y:
-    Case 1: x > y
-      Since x > y we have (max x y) = x
-    Case 2: x ≤ y
-      Since x ≤ y we have (max x y) = y
-QED
-
-Example "Using cases to prove max"
-  Given:
-  Assume:
-  Conclusion: ∀ (x y: ℝ), (max x y) = x ∨ (max x y) = y
-Proof:
-  with_panel_widgets [GoalTypePanel]
-  Fix x y
-  We discuss depending on whether x ≥ y ∨ x < y
-    Assume h : x ≥ y
-    Let's prove that (max x y) = x
-      Since x ≥ y we have (max x y) = x
-    Assume x < y
-    Let's prove that (max x y) = y
-      Since x < y we have (max x y) = y
-
-  Consider cases on whether x ≥ y:
-    Since h we have that max x y = x
-  else
-    Since ¬h we have max x y = y
-QED

ff9552fbdc66b3ce67ffd49532a73f41d8bd51bb
Pim Otte <pim@otte.dev>
2025-12-01 13:29:24 +0100

    wip

---
+++ cypress/e2e/hello.cy.ts
@@ -0,0 +1,7 @@
+ it('Should execute Hello World command', () => {
+    //open the command palette
+    //type Hello World and enter
+    // assert that the notification is opened.
+    cy.visit("http://localhost:3000/");
+     
+ })

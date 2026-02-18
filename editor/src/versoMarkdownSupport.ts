e680e8a890c2da957f6ce7543f8b16198306531e
DikieDick <26772815+DikieDick@users.noreply.github.com>
2026-02-18 13:49:47 +0100

    Add documentation

--- editor/src/versoMarkdownSupport.ts
+++ editor/src/versoMarkdownSupport.ts
@@ -1,7 +1,10 @@
 import { defaultToMarkdown } from "@impermeable/waterproof-editor";
 
-
+/**
+ * Translates the Markdown from the Waterproof Lean verso genre into markdown compatible syntax.
+ * @param input A string of Markdown in the Waterproof Lean verso genre.
+ * @returns A string of valid Markdown with inline math replaced.
+ */
 export function versoMarkdownToMarkdown(input: string): string {
-    console.log("WWOOOOOOO");
     return defaultToMarkdown(input.replaceAll(/\$`([\s\S]*?)`/g, "$$$1$$"));
 }
